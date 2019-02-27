cad34d15878a3514b3e73b45dec8d38136de6438
Masoud Tavakkoli <mtg1376@gmail.com>
2019-02-27 19:16:07 +0330

    read file from sd

--- App.js
+++ App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Platform, StyleSheet, Text, View, Dimensions, Image, AsyncStorage } from 'react-native';
 import { WebView } from "react-native-webview";
+import RNFetchBlob from 'rn-fetch-blob'
 
 const { width, height } = Dimensions.get('screen')
+const dirs = RNFetchBlob.fs.dirs
+let imageView;
 export default class App extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			image: null
+		}
+	}
+
+	async componentDidMount() {
+		// let ls = await RNFetchBlob.fs.ls(dirs.DownloadDir)
+		// console.log(dirs);
+		// console.log(dirs);
+		// console.log(dirs.DownloadDir);
+
+		RNFetchBlob
+			.config({
+				path: dirs.DocumentDir + "/sds1.mp4",
+				// fileCache: true,
+				// appendExt: 'mp4'
+			})
+			.fetch('GET', 'https://www.w3schools.com/html/mov_bbb.mp4', {
+				filename: "1",
+			})
+			.then(async (res) => {
+				// the temp file path with file extension `png`
+				console.log('The file saved to ', res.path())
+				await AsyncStorage.setItem("image", res.path())
+				this.setState({ image: res.path() })
+			});
+		this.setState({ image: await AsyncStorage.getItem('image') })
+	}
+
 	render() {
+		console.log(this.state.image);
+
 		return (
 			<View style={styles.container}>
 				<WebView allowFileAccess={true} scalesPageToFit style={{ width }} source={{
-					baseUrl: "",
-					html: `<video width="${width}" height="${height * 0.8}" controls>
-					<source src="file:///android_asset/vi.mp4" type="video/mp4">
+					baseUrl: "/",
+					html: `<video width="${width}" height="${height * 0.5}" controls>
+					<source src="file://${this.state.image}" type="video/mp4">
 					</video>`
 				}} />
+				{/* {this.state.image} */}
 			</View>
 		);
 	}
--- android/app/src/main/java/com/digikala/MainApplication.java
+++ android/app/src/main/java/com/digikala/MainApplication.java
@@ -3,6 +3,7 @@ package com.digikala;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.react.SmsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new RNCWebViewPackage(),
             new SmsPackage(),
             new VectorIconsPackage()
