a323aa655a8d40cc0ece27e4d34ae889477b3a77
Masoud Tavakkoli <mtg1376@gmail.com>
2019-02-27 12:46:05 +0330

    added video playing

--- App.js
+++ App.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Dimensions } from 'react-native';
+import { WebView } from "react-native-webview";
 
+const { width, height } = Dimensions.get('screen')
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<WebView allowFileAccess={true} scalesPageToFit style={{ width }} source={{
+					baseUrl: "",
+					html: `<video width="${width}" height="${height * 0.8}" controls>
+					<source src="file:///android_asset/vi.mp4" type="video/mp4">
+					</video>`
+				}} />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
 });
